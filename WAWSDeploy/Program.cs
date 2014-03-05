dee3955c3b86f7910aa7efff689937dfbdd7d788
davidebbo <david.ebbo@microsoft.com>
2014-03-05 11:41:02 -0800

    Initial commit

---
+++ WAWSDeploy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Web.Deployment;
+
+namespace WAWSDeploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Syntax: WAWSDeploy.exe FolderToDeploy MySite.PublishSettings");
+                return;
+            }
+
+            try
+            {
+                var webDeployHelper = new WebDeployHelper();
+                Console.WriteLine("Starting deployment...");
+                DeploymentChangeSummary changeSummary = webDeployHelper.DeployContentToOneSite(args[0], args[1]);
+
+                Console.WriteLine("BytesCopied: {0}", changeSummary.BytesCopied);
+                Console.WriteLine("Added: {0}", changeSummary.ObjectsAdded);
+                Console.WriteLine("Updated: {0}", changeSummary.ObjectsUpdated);
+                Console.WriteLine("Deleted: {0}", changeSummary.ObjectsDeleted);
+                Console.WriteLine("Errors: {0}", changeSummary.Errors);
+                Console.WriteLine("Warnings: {0}", changeSummary.Warnings);
+                Console.WriteLine("Total changes: {0}", changeSummary.TotalChanges);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Deployment failed: {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ WAWSDeploy/WebDeployHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Web.Deployment;
+
+namespace WAWSDeploy
+{
+    public class WebDeployHelper
+    {
+        public DeploymentChangeSummary DeployContentToOneSite(string contentPath, string publishSettingsFile)
+        {
+            var sourceBaseOptions = new DeploymentBaseOptions();
+            DeploymentBaseOptions destBaseOptions;
+            string siteName = ParsePublishSettings(publishSettingsFile, out destBaseOptions);
+
+            Trace.TraceInformation("Starting WebDeploy for {0}", Path.GetFileName(publishSettingsFile));
+
+            // Publish the content to the remote site
+            using (var deploymentObject = DeploymentManager.CreateObject(DeploymentWellKnownProvider.ContentPath, contentPath, sourceBaseOptions))
+            {
+                // Note: would be nice to have an async flavor of this API...
+                return deploymentObject.SyncTo(DeploymentWellKnownProvider.ContentPath, siteName, destBaseOptions, new DeploymentSyncOptions());
+            }
+        }
+
+        private string ParsePublishSettings(string path, out DeploymentBaseOptions deploymentBaseOptions)
+        {
+            var document = XDocument.Load(path);
+            var profile = document.Descendants("publishProfile").First();
+
+            string siteName = profile.Attribute("msdeploySite").Value;
+
+            deploymentBaseOptions = new DeploymentBaseOptions
+            {
+                ComputerName = String.Format("https://{0}/msdeploy.axd?site={1}", profile.Attribute("publishUrl").Value, siteName),
+                UserName = profile.Attribute("userName").Value,
+                Password = profile.Attribute("userPWD").Value,
+                AuthenticationType = "Basic"
+            };
+
+            return siteName;
+        }
+    }
+}
