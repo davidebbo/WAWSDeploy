444d9a90c154088502c5ac50650cd96063b2c379
Chris Kirby <chris@infinitusdev.com>
2014-03-06 09:16:08 -0800

    Added optional password argument and tracing - added Args Nuget package and DeploymentArgs class for argument parsing - added a default console trace listener and replace console writes with trace writes. - modified helper to use the arg password over the pub settings password when specified.

---
+++ WAWSDeploy/DeploymentArgs.cs
@@ -0,0 +1,31 @@
+﻿using Args;
+
+namespace WAWSDeploy
+{
+	/// <summary>
+	/// Arguments used for publishing
+	/// </summary>
+	public class DeploymentArgs
+	{
+		/// <summary>
+		/// Gets or sets the folder.
+		/// </summary>
+		/// <value>The folder.</value>
+		[ArgsMemberSwitch(0)]
+		public string Folder { get; set; }
+
+		/// <summary>
+		/// Gets or sets the publish settings file.
+		/// </summary>
+		/// <value>The publish settings file.</value>
+		[ArgsMemberSwitch(1)]
+		public string PublishSettingsFile { get; set; }
+
+		/// <summary>
+		/// Gets or sets the password.
+		/// </summary>
+		/// <value>The password.</value>
+		[ArgsMemberSwitch("p", "password", "pw")]
+		public string Password { get; set; }
+	}
+}
